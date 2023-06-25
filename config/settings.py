66cdac27be32c3fc01a6fdf9b4afa77b82ac3cbb
jongmee <ra123221@gmail.com>
2023-06-25 13:54:55 +0900

    #1 Add: 커스텀 유저 모델 생성

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 유저 모델 설정
+AUTH_USER_MODEL = 'accounts.User'
