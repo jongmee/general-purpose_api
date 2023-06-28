a3198576b97381bd042146b36b0a7b2504c5eef4
jongmee <ra123221@gmail.com>
2023-06-28 21:22:15 +0900

    #1 Add: 구글 소셜 로그인 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import RegisterAPIView, UserAPIView
+from .views import RegisterAPIView, UserAPIView, google_login, google_callback, GoogleLogin
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = routers.SimpleRouter()
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', UserAPIView.as_view(), name='user_api'),
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('refresh/', TokenRefreshView.as_view()), # jwt 토큰 재발급
+    path('allauth/', include('allauth.urls')),
+    path('google/login/', google_login, name='google_login'),
+    path('google/callback/', google_callback, name='google_callback'),
+    path('google/login/finish/', GoogleLogin.as_view(), name='google_login_todjango'),
 ]
